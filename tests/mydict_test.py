7ce7c17fa6e304564c79619b2e13d27f8f4457c6
zhanguanzhao <40819522+zhanguanzhao@users.noreply.github.com>
2018-07-16 14:23:12 +0800

    Update mydict_test.py

--- tests/mydict_test.py
+++ tests/mydict_test.py
@@ -9,7 +9,7 @@ class TestDict(unittest.TestCase):
 
     def test_init(self):
         d = Dict(a=1, b='test')
-        self.assertEqual(d.a, 1)
+        self.assertEqual(d.a, 2)
         self.assertEqual(d.b, 'test00')
         self.assertTrue(isinstance(d, dict))
 
